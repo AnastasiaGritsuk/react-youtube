b3141a760245872648a54b5f9cbf6e7c93c713e2
AnastasiaGritsuk <natikana_@mail.ru>
2020-07-08 21:16:14 +0300

    version with useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
-  return <div />
+  const [todos, setTodos] = React.useState([
+    { id: 1, completed: false, title: "Buy bread" },
+    { id: 2, completed: true, title: "Buy oil" },
+    { id: 3, completed: false, title: "Buy milk" },
+  ]);
+
+  function toggleToDo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title: title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo} />
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleToDo} />
+        ) : (
+          <p>No todos</p>
+        )}
+      </div>
+    </Context.Provider>
+  );
 }
 
 export default App;
